b84bf4f4a9d8fcce43b8e441b297b92e7b6c38fc
Unknown <dnacharaju@blurpd.com>
2018-06-12 09:25:50 -0400

    Iris Detection Function

---
+++ ProcessBlink.m
@@ -0,0 +1,25 @@
+%% Initialize Workspace
+clear all
+close all
+%% Download video 
+tic
+Clip = 'blink419.avi';
+clip = VideoReader(Clip);
+Switch = 0;
+while hasFrame(clip)
+    video = readFrame(clip);
+    video = imgaussfilt(video,2);
+    image(video);
+    pause(1/clip.FrameRate);
+    [out] = PupilOverlay(video,0);
+    if out == 0 && Switch == 0
+        fprintf('Full Blink \n')
+        Switch = 1;
+        toc
+        return
+    end
+end
+if Switch == 0
+    fprintf('Partial Blink \n')
+    toc
+end
