dca181ff1b5dc242d000fdc75211056d5c72df0d
Unknown <esimons@blurpd.com>
2018-06-13 16:44:26 -0400

    blob test x2

--- PupilOverlay.m
+++ PupilOverlay.m
@@ -1,114 +1,100 @@
 function [out,centers,radii,mask] = PupilOverlay(eye,plot,oldcenter)
 out = 1;
-%eye =rgb2gray(eye);
+%eye = rgb2gray(eye);
 %eye = adapthisteq(eye,'clipLimit',0.02,'Distribution','rayleigh'); 
-eye2=eye;
- 
+eye2 = eye;
 mask = eye2;
+counter = 0;
+centers = [];
+radii = [];
+centerThresh = 250;
  
 if plot ==1
- 
     figure()
- 
     clf
- 
     subplot(1,3,1)
- 
     imshow(eye)
- 
     subplot(1,3,2)
- 
     imshow(eye)
- 
 end
  
 %tic
-[allcenters,allradii]=imfindcircles(eye,[200, 300],'ObjectPolarity','dark','Sensitivity',0.96,'EdgeThreshold',0.05,'Method','twostage');
+[allcenters,allradii] = imfindcircles(eye,[160, 310],'ObjectPolarity','dark',...
+    'Sensitivity',0.95,'EdgeThreshold',0.05,'Method','twostage');
 %toc
-whos allcenters
- counter = 0;
- centers =[];
- radii = [];
- if numel(oldcenter)>0
+%whos allcenters
+
+if numel(oldcenter)>0
     for k = 1:1:size(allcenters,1)
-        if sqrt((allcenters(k,1)-oldcenter(1)).^2 + (allcenters(k,2)-oldcenter(2)).^2) < 100
-            centers = [centers; allcenters(k,1), allcenters(k,2)];
-            radii = [radii; allradii(k)];
-            fprintf('Off Center Omission\n')
+        if sqrt((allcenters(k,1)-oldcenter(1)).^2 + (allcenters(k,2)-oldcenter(2)).^2) < 75
+           centers = [centers; allcenters(k,1), allcenters(k,2)];
+           radii = [radii; allradii(k)];
+           fprintf('Center within Range\n')
         end
     end
- else
+else
      centers = allcenters;
      radii = allradii;
- end
+end
+
+if numel(centers) >= 2
+    for k = 1:size(centers(:,1))
+        if centers(k,1) < 832+centerThresh && centers(k,1) > 832-centerThresh %...
+%                && centers(k,2) <= (832-200)
+           continue
+        else
+            centers(k,:) = [0];
+            fprintf('Omitted Off-Center Circle\n')
+        end
+    end
+end
+
 while numel(centers) > 2 && counter < 5
- [centers,radii]=imfindcircles(eye,[200, 300],'ObjectPolarity','dark','Sensitivity',0.95-counter*0.01,'EdgeThreshold',0.05,'Method','twostage');
-counter = counter +1;
-end    
+    [centers,radii] = imfindcircles(eye,[160, 310],'ObjectPolarity','dark',...
+        'Sensitivity',0.95-(counter+1)*0.01,'EdgeThreshold',0.05,'Method','twostage');
+    counter = counter + 1;
+    fprintf('counter = %d\n',counter)
+end 
+
 if counter > 4
-    fprintf('Number of Circles Error')
+    fprintf('Number of Circles Error\n')
     out = 0;
     return
 end
+
 if numel(centers) < 2
-    fprintf('No Visible Iris \n')
- 
+    fprintf('No Visible Iris\n')
     out = 0;
- 
     return
- 
 end
  
 %%
  
 if plot == 1
- 
     h = viscircles(centers,radii);
- 
     imagesizex = size(eye,1);
- 
     imagesizey = size(eye,2);
- 
     [colImage, rowImage] = meshgrid(1:imagesizex,1:imagesizey);
- 
     circlepixels = zeros(size(eye));
- 
     z = circlepixels;
- 
     centerY = ceil(centers(1));
- 
     centerX = ceil(centers(2));
- 
     ind = sub2ind(size(eye),centerX,centerY);
- 
     radius = radii(1);
- 
     circlepixels((rowImage- centerY).^2 + (colImage-centerX).^2 <= radius^2) =1;
  
     %% 
- 
     h = fspecial('disk',radius);
- 
     thresh = 10000000;
- 
     h(h~=0) = thresh;
- 
     hsize = size(h);
- 
     eye(ceil(centerX-hsize(1)./2):ceil(centerX+hsize(1)./2)-1,ceil(centerY-hsize(2)./2):ceil(centerY+hsize(2)./2)-1)= h;
- 
     subplot(1,3,3)
- 
     eye2(eye==255)= 255;
- 
     imshow(eye2)
- 
     axis tight
- 
     colormap gray
- 
     figure()
- 
     mask(eye~=255)=0;
     %mask = rgb2gray(mask);
     imshow(mask)
